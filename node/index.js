b5549279046eb108c4343fc948550d7d51f2a004
Akash Jha <akashjhasciencea@gmail.com>
2024-11-24 19:02:28 +0530

    Update index.js
    syntaxerror

--- node/index.js
+++ node/index.js
@@ -167,4 +167,4 @@ process.on('SIGINT', () => {
 server.listen(process.env.PORT || 8000, '0.0.0.0', () => {  
     console.log(`Server is listening on port ${process.env.PORT || 8000}`);  
 });  
-});
+
